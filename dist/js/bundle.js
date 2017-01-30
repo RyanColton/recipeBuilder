0e2ce4affcb57432443092a41631bfa99dbbb0a0
RyanColton <ryan.m.colton@gmail.com>
2017-01-30 09:32:23 -0700

    additional content

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -36,93 +36,6 @@ angular.module('recipeApp', ['ui.router', 'chart.js', 'ui.grid', 'tc.chartjs']).
     }
   });
 });
-'use strict';
-
-angular.module('recipeApp').service('mainService', function ($http, $q) {
-  this.searchFoodList = function (searchTerm) {
-    return $http({
-      method: 'GET',
-      url: 'http://api.nal.usda.gov/ndb/search/?format=json&q=' + searchTerm + '&sort=r&max=30&ds=Standard Reference&api_key=gSF86FdjQ4LaECs7MXJ9m9aHjs5q8E4I4oKiw3uh&nutrients=205&nutrients=204&nutrients=208&nutrients=269'
-    });
-  };
-
-  this.foodId = 0;
-
-  this.nutritionData = function () {
-    return $http({
-      method: 'GET',
-      url: 'http://api.nal.usda.gov/ndb/reports/?ndbno=' + this.foodId + '&type=b&format=json&api_key=gSF86FdjQ4LaECs7MXJ9m9aHjs5q8E4I4oKiw3uh'
-    });
-  };
-
-  this.dailyRecomendedValue = [{
-    nutrient: 'calcium',
-    nutrientId: 301,
-    value: 1000
-  }, {
-    nutrient: 'iron',
-    nutrientId: 303,
-    value: 18
-  }, {
-    nutrient: 'vitamin C',
-    nutrientId: 401,
-    value: 60
-  }, {
-    nutrient: 'sodium',
-    nutrientId: 307,
-    value: 2400
-  }, {
-    nutrient: 'magnesium',
-    nutrientId: 304,
-    value: 400
-  }, {
-    nutrient: 'phosphorus',
-    nutrientId: 305,
-    value: 1000
-  }, {
-    nutrient: 'potassium',
-    nutrientId: 306,
-    value: 3500
-  }, {
-    nutrient: 'zinc',
-    nutrientId: 309,
-    value: 15
-  }, {
-    nutrient: 'thiamin',
-    nutrientId: 404,
-    value: 1.5
-  }, {
-    nutrient: 'riboflavin',
-    nutrientId: 405,
-    value: 1.5
-  }, {
-    nutrient: 'niacin',
-    nutrientId: 406,
-    value: 20
-  }, {
-    nutrient: 'vitamin B-6',
-    nutrientId: 415,
-    value: 2
-  }];
-});
-'use strict';
-
-angular.module('recipeApp').directive('searchDir', function () {
-  return {
-    restirct: 'E',
-    templateUrl: './js/directives/direcTemps/searchDir.html',
-    controller: 'searchDirCtrl'
-  };
-});
-'use strict';
-
-angular.module('recipeApp').directive('searchResultDir', function () {
-  return {
-    restirct: 'E',
-    templateUrl: './js/directives/direcTemps/searchResultDir.html',
-    controller: 'foodSearchResultsController'
-  };
-});
 "use strict";
 'use strict';
 
@@ -326,6 +239,93 @@ angular.module('recipeApp').controller('foodSearchResultsController', function (
 angular.module('recipeApp').controller('homeController', function ($scope, mainService) {});
 "use strict";
 "use strict";
+'use strict';
+
+angular.module('recipeApp').directive('searchDir', function () {
+  return {
+    restirct: 'E',
+    templateUrl: './js/directives/direcTemps/searchDir.html',
+    controller: 'searchDirCtrl'
+  };
+});
+'use strict';
+
+angular.module('recipeApp').directive('searchResultDir', function () {
+  return {
+    restirct: 'E',
+    templateUrl: './js/directives/direcTemps/searchResultDir.html',
+    controller: 'foodSearchResultsController'
+  };
+});
+'use strict';
+
+angular.module('recipeApp').service('mainService', function ($http, $q) {
+  this.searchFoodList = function (searchTerm) {
+    return $http({
+      method: 'GET',
+      url: 'http://api.nal.usda.gov/ndb/search/?format=json&q=' + searchTerm + '&sort=r&max=30&ds=Standard Reference&api_key=gSF86FdjQ4LaECs7MXJ9m9aHjs5q8E4I4oKiw3uh&nutrients=205&nutrients=204&nutrients=208&nutrients=269'
+    });
+  };
+
+  this.foodId = 0;
+
+  this.nutritionData = function () {
+    return $http({
+      method: 'GET',
+      url: 'http://api.nal.usda.gov/ndb/reports/?ndbno=' + this.foodId + '&type=b&format=json&api_key=gSF86FdjQ4LaECs7MXJ9m9aHjs5q8E4I4oKiw3uh'
+    });
+  };
+
+  this.dailyRecomendedValue = [{
+    nutrient: 'calcium',
+    nutrientId: 301,
+    value: 1000
+  }, {
+    nutrient: 'iron',
+    nutrientId: 303,
+    value: 18
+  }, {
+    nutrient: 'vitamin C',
+    nutrientId: 401,
+    value: 60
+  }, {
+    nutrient: 'sodium',
+    nutrientId: 307,
+    value: 2400
+  }, {
+    nutrient: 'magnesium',
+    nutrientId: 304,
+    value: 400
+  }, {
+    nutrient: 'phosphorus',
+    nutrientId: 305,
+    value: 1000
+  }, {
+    nutrient: 'potassium',
+    nutrientId: 306,
+    value: 3500
+  }, {
+    nutrient: 'zinc',
+    nutrientId: 309,
+    value: 15
+  }, {
+    nutrient: 'thiamin',
+    nutrientId: 404,
+    value: 1.5
+  }, {
+    nutrient: 'riboflavin',
+    nutrientId: 405,
+    value: 1.5
+  }, {
+    nutrient: 'niacin',
+    nutrientId: 406,
+    value: 20
+  }, {
+    nutrient: 'vitamin B-6',
+    nutrientId: 415,
+    value: 2
+  }];
+});
 'use strict';
 
 angular.module('recipeApp').controller('searchDirCtrl', function ($scope, $state, mainService, uiGridConstants) {
